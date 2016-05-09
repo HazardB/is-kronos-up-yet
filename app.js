10aa0e6acb0d5306f19fa21426bebaa67833284a
Hazard <hazard@noreply.com>
2016-05-09 22:12:34 +0100

    reverting port to 3000

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ var compression = require('compression');
 var app = express();
 
 app.use(compression());
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 3000);
 
 // TODO: add debug config to change this to 3000
