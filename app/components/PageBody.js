ac974c844d17d54d3c72346ab60138b7e241673e
Hazard <hazard@noreply.com>
2016-05-17 23:39:01 +0100

    Changing to red - kronos down... need to automate this part.

--- app/components/PageBody.js
+++ app/components/PageBody.js
@@ -14,11 +14,11 @@ var PageBody = React.createClass({
   render: function(){
     return (
       <div>
-      <Alert bsStyle="warning">
-          <img src="img/mind-blown.gif" alt="Kronos is experiencing problems!" className="center-block img-responsive"></img>
+      <Alert bsStyle="danger">
+          <img src="img/down.gif" alt="Kronos is experiencing problems!" className="center-block img-responsive"></img>
 
-          <h2 className="text-center">Kronos is still experiencing some latency issues</h2>
-          <p className="text-center">The server might pop up and down over the next few days. Thanks to TwinStar for the continued effort.</p>
+          <h2 className="text-center">Kronos is DOWN!</h2>
+          <p className="text-center">Kronos is now experiencing issues after the server move. Keep your eyes peeled here for live updates to the server status. </p>
           <Row>
           <Col xs={4} xsOffset={8}>
           <Button bsStyle="warning" bsSize="xsmall" className="text-center" href="http://www.kronos-wow.com/2016/05/16/maintenance/" target="_blank">Latest announcement</Button>
