4d769cd55647bbf74f0ebd43a9bc59aa07003d16
Hazard <hazard@noreply.com>
2016-05-18 14:41:47 +0100

    Reinstating the gif changing functionality

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Navigation = require('./Navigation');
+var ServerStatus = require('./ServerStatus');
 var PageBody = require('./PageBody');
 var Social = require('./Social');
 var Footer = require('./Footer');
@@ -12,6 +13,7 @@ var Main = React.createClass({
       <div className="container">
         <Navigation />
         <PageBody />
+        <ServerStatus />
         <Social />
         <Footer />
       </div>
--- app/components/PageBody.js
+++ app/components/PageBody.js
@@ -11,21 +11,34 @@ var Button  = ReactBootstrap.Button,
     Checkbox  = ReactBootstrap.Checkbox;
 
 var PageBody = React.createClass({
+  getInitialState: function() {
+    return { gif: 'down.gif'};
+  },
+
+  changeGif: function(){
+    var newGif = this.randomiseGif();
+    while(newGif == this.state.gif){
+      newGif = this.randomiseGif();
+    }
+
+    this.setState({ gif: newGif});
+  },
+  randomiseGif: function(){
+    var availableImages = [ 'down.gif', 'fuck.gif', 'sad.gif', 'sp-down.gif', 'drink-swirl.gif', 'dumb-and-dumber.gif', 'homer-monkey.gif', 'the-doctor.gif', 'rare-pepe.jpg'];
+    var rand = Math.floor(Math.random() * availableImages.length);
+    return availableImages[rand];
+  },
   render: function(){
+    var gif = this.state.gif;
+
     return (
       <div>
       <Alert bsStyle="warning">
-          <img src="img/down.gif" alt="Kronos is experiencing problems!" className="center-block img-responsive"></img>
+        <img src={"img/" + gif} alt="Kronos is experiencing problems!" className="main-image center-block img-responsive" onClick={this.changeGif}></img>
 
           <h2 className="text-center">Kronos is still experiencing issues!</h2>
           <p className="text-center">Kronos is having network issues after the server move. Keep your eyes peeled here for live updates to the server status. </p>
-          <Row>
-          <Col xs={4} xsOffset={8}>
-          <Button bsStyle="warning" bsSize="xsmall" className="text-center" href="http://www.kronos-wow.com/2016/05/16/maintenance/" target="_blank">Latest announcement</Button>
-          </Col>
-          </Row>
       </Alert>
-      <ServerStatus />
   </div>
     )
   }
